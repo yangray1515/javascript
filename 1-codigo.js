3b6a9501798fd8775d32e9f3446e0069bd885570
yangray1515 <102757737+yangray1515@users.noreply.github.com>
2022-05-05 13:44:12 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,7 @@
+console.log('Olá, mundo!');
+console.log(7+4);
+console.log('7'+'4');
+console.log(12+20);
+console.log(25+157);
+console.log(30+16);
+console.log('Raiana '+ 'Nascimento');
---
+++ exercicios.js
@@ -0,0 +1,32 @@
+//1-a
+console.log( 10 + 20 * 30);
+//console.log(10+600);
+//console.log(610);
+ //1-b
+ console.log(4**2/30);
+
+ //1-c
+ console.log((9**4+2)*6-1);
+
+ //2
+ console.log(10%3*10**1+1-10*4/2);
+
+ //3
+ console.log('Raiana');
+
+ //4
+ const x= 3;
+const y= 5;
+console.log(2*x*3*y)
+
+//5
+const d= 2;
+const e= 3;
+const f=4;
+console.log(d+e+f)
+
+//6
+const salario =1800;
+const dobra =2;
+console.log(salario*dobra);
+
